39037e40d9bfd24a64bb3403fd73303455765fc1
Marcio Duarte <marcioadr88@gmail.com>
2024-11-30 00:19:57 -0300

    Create XCode project with initial configuration

---
+++ Clara Mobile Challenge/Clara Mobile Challenge/ClaraMobileChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaraMobileChallengeApp.swift
+//  Clara Mobile Challenge
+//
+//  Created by Marcio Duarte on 2024-11-30.
+//
+
+import SwiftUI
+
+@main
+struct ClaraMobileChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
