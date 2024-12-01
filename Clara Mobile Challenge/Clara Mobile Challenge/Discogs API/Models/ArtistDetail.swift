323134db7739acc848680351cf844be18b5d72bf
Marcio Duarte <marcioadr88@gmail.com>
2024-11-30 22:14:05 -0300

    add custom decoding for ArtistDetail

--- Clara Mobile Challenge/Clara Mobile Challenge/Discogs API/Models/ArtistDetail.swift
+++ Clara Mobile Challenge/Clara Mobile Challenge/Discogs API/Models/ArtistDetail.swift
@@ -17,3 +17,36 @@ struct ArtistDetail: Decodable {
     let members: [BandMember]?
 }
 
+extension ArtistDetail {
+    enum CodingKeys: String, CodingKey {
+        case id, name, profile, urls, members
+        case imageURL = "images"
+        case releasesURL = "releases_url"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        id = try container.decode(Int.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        profile = try container.decodeIfPresent(String.self, forKey: .profile)
+        releasesURL = try container.decode(String.self, forKey: .releasesURL)
+        urls = try container.decodeIfPresent([String].self, forKey: .urls)
+        members = try container.decodeIfPresent([BandMember].self, forKey: .members)
+        
+        if let images = try container.decodeIfPresent([Image].self, forKey: .imageURL),
+           let firstImage = images.first {
+            imageURL = firstImage.resourceURL
+        } else {
+            imageURL = nil
+        }
+    }
+}
+
+private struct Image: Codable {
+    let resourceURL: String
+    
+    enum CodingKeys: String, CodingKey {
+        case resourceURL = "resource_url"
+    }
+}
