05a819bebd8d8a1fb38a8eef809eb000d18250da
Marcio Duarte <marcioadr88@gmail.com>
2024-12-02 02:47:36 -0300

    Add Localizables helper

---
+++ Clara Mobile Challenge/Clara Mobile Challenge/Util/Localizables.swift
@@ -0,0 +1,29 @@
+//
+//  Localizables.swift
+//  Clara Mobile Challenge
+//
+//  Created by Marcio Duarte on 2024-12-02.
+//
+
+import Foundation
+
+@dynamicMemberLookup
+final class Localizables {
+    private init() {}
+    
+    private static let stringsName = "Strings"
+    
+    static subscript(dynamicMember member: String) -> String {
+        NSLocalizedString(member, 
+                          tableName: stringsName,
+                          bundle: Bundle.main,
+                          comment: "")
+    }
+    
+    static subscript(dynamicMember member: String) -> (_ arguments: CVarArg...) -> String {
+        { arguments in
+            String(format: Localizables[dynamicMember: member],
+                   arguments: arguments)
+        }
+    }
+}
